e2ba202567811e4064c6dac6d22cb7623f7b690a
a.savanovich <asavanster@gmail.com>
2020-06-28 08:46:52 +0300

    pass socket url as param

--- app/src/main/java/xyz/atenalp/riverfight/android/AndroidWebServerActivity.java
+++ app/src/main/java/xyz/atenalp/riverfight/android/AndroidWebServerActivity.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.text.format.Formatter;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,11 +11,19 @@ import android.widget.TextView;
 import com.google.androidbrowserhelper.trusted.LauncherActivity;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 public class AndroidWebServerActivity extends Activity {
     private static final int STATIC_CONTENT_PORT = 8080;
     private static final int WEB_SOCKET_PORT = 8088;
+    private static final String WEB_GAME_URL = "https://atenalp.xyz";
     public static final String LOCAL_IP = "127.0.0.1";
     private AndroidStaticAssetsServer server;
     private WebSocketBroadcastServer webSocketServer;
@@ -28,11 +33,18 @@ public class AndroidWebServerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Context applicationContext = getApplicationContext();
-        WifiManager wifiManager = (WifiManager) applicationContext.getSystemService(WIFI_SERVICE);
 
-        final String host = getHostOrLocal(wifiManager);
+        String formattedIpAddress = getIPAddress();
+        final String host;
+        if (formattedIpAddress != null) {
+            host = getStaticHost(formattedIpAddress);
+        } else {
+            host = getStaticHost(LOCAL_IP);
+        }
+        String webSocketHost = getSocketHost(formattedIpAddress);
         TextView textIpAddress = (TextView) findViewById(R.id.ipaddr);
         textIpAddress.setText(getString(R.string.please, host));
+
         try {
             server = new AndroidStaticAssetsServer(applicationContext, STATIC_CONTENT_PORT, "www");
             if (!isHostLocal(host)) {
@@ -40,66 +52,96 @@ public class AndroidWebServerActivity extends Activity {
                 webSocketServer.start(0);
             }
             addButton(host);
-            addButton2(host);
-            launchTwa(host, applicationContext);
+            addButton2(host, applicationContext);
+            addButton3(WEB_GAME_URL, webSocketHost, host, applicationContext);
+            launchTwa(host, webSocketHost, host, applicationContext);
         } catch (IOException e) {
-            e.printStackTrace();
+            TextView textIpAddress2 = (TextView) findViewById(R.id.ipaddr2);
+            textIpAddress2.setText(Arrays.toString(e.getStackTrace()));
         }
     }
 
     private void addButton(final String host) {
         Button btn = (Button) findViewById(R.id.button1);
         btn.setOnClickListener(v -> {
-            Uri launchUri = Uri.parse(getLaunchUrl(host));
+            Uri launchUri = Uri.parse(getLaunchUrl(host, null, null));
             startActivity(new Intent(Intent.ACTION_VIEW, launchUri));
         });
     }
 
-    private void addButton2(final String host) {
+    private void addButton2(final String host, Context context) {
         Button btn = (Button) findViewById(R.id.button2);
-        btn.setOnClickListener(v -> launchTwa(host, getApplicationContext()));
+        btn.setOnClickListener(v -> launchTwa(host, null, null, context));
     }
 
-    private void launchTwa(String host, Context context) {
-        Uri launchUri = Uri.parse(getLaunchUrl(host));
+    private void addButton3(final String host, String socketHost, String staticHost, Context context) {
+        Button btn = (Button) findViewById(R.id.button3);
+        btn.setOnClickListener(v -> launchTwa(host, socketHost, staticHost, context));
+    }
+
+    private void launchTwa(String host, String socketHost, String staticHost, Context context) {
+        Uri launchUri = Uri.parse(getLaunchUrl(host, socketHost, staticHost));
         startActivity(new Intent(Intent.ACTION_VIEW, launchUri, context, LauncherActivity.class));
     }
 
-    private static String getLaunchUrl(String host) {
-        if (isHostLocal(host)) {
-            return host;
+    private String getLaunchUrl(String host, String socketHost, String staticHost) {
+        StringBuilder b = new StringBuilder();
+        b.append(host);
+        b.append("/?color=red");
+        try {
+            if (socketHost != null) {
+                b.append("&wh=");
+                b.append(URLEncoder.encode(socketHost, StandardCharsets.UTF_8.toString()));
+            }
+
+            if (staticHost != null) {
+                b.append("&sh=");
+                b.append(URLEncoder.encode(staticHost, StandardCharsets.UTF_8.toString()));
+            }
+        } catch (Exception ignored) {
+
         }
-        return host + "/?color=red";
+        TextView textIpAddress2 = (TextView) findViewById(R.id.ipaddr2);
+        textIpAddress2.setText(b.toString());
+        return b.toString();
     }
 
-    private static String getHostOrLocal(WifiManager wifiManager) {
-        String formattedIpAddress = getNullableHost(wifiManager);
-        if (formattedIpAddress != null) return formattedIpAddress;
-        return getHost(LOCAL_IP);
-    }
-
-    private static String getNullableHost(WifiManager wifiManager) {
-        if (wifiManager == null) {
-            return null;
-        }
-        WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-        if (connectionInfo == null) {
-            return null;
-        }
-        int ipAddress = connectionInfo.getIpAddress();
-        if (ipAddress == 0) {
-            return null;
-        }
-        String formattedIpAddress = Formatter.formatIpAddress(ipAddress);
-        return getHost(formattedIpAddress);
-    }
 
     private static boolean isHostLocal(String host) {
         return host.contains(LOCAL_IP);
     }
 
-    private static String getHost(String ip) {
+
+    public static String getIPAddress() {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+
+            for (NetworkInterface interface_ : interfaces) {
+                for (InetAddress inetAddress : Collections.list(interface_.getInetAddresses())) {
+                    if (inetAddress.isLoopbackAddress()) {
+                        continue;
+                    }
+
+                    String ipAddr = inetAddress.getHostAddress();
+                    boolean isIPv4 = ipAddr.indexOf(':') < 0;
+                    if (!isIPv4) {
+                        continue;
+                    }
+                    return ipAddr;
+                }
+
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
+    }
+
+    private static String getStaticHost(String ip) {
         return "http://" + ip + ":" + STATIC_CONTENT_PORT;
+    }
+
+    private static String getSocketHost(String ip) {
+        return ip + ":" + WEB_SOCKET_PORT;
     }
 
     @Override
